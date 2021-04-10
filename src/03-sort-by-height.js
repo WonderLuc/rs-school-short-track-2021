7b286d3f89c60d998065512d2ddd4a0712e1dd6c
wonderLuc <workmaildo@yandex.ru>
2021-04-10 12:29:46 +0500

    feat: slove task 03

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,28 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  // isModifed set to true for first-time check
+  const resultArr = arr;
+  let isModifed = true;
+  while (isModifed) {
+    isModifed = false;
+    for (let i = 0, currentOffset = 1; i < resultArr.length; i++) {
+      if (resultArr[i] !== -1) {
+        while (resultArr[currentOffset] === -1) {
+          currentOffset++;
+        }
+        if (resultArr[i] > resultArr[currentOffset]) {
+          const temp = resultArr[i];
+          resultArr[i] = resultArr[currentOffset];
+          resultArr[currentOffset] = temp;
+          isModifed = true;
+        }
+      }
+      currentOffset = i + 2;
+    }
+  }
+  return resultArr;
 }
 
 module.exports = sortByHeight;

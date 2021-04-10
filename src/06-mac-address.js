f9187f2efd8c511cf32ffecfec08c2030477b9c0
wonderLuc <workmaildo@yandex.ru>
2021-04-10 23:58:11 +0500

    feat: slove task 06

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,8 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  return n.match(/[0-9A-Fa-f-]+/)[0].length === 17;
 }
 
 module.exports = isMAC48Address;

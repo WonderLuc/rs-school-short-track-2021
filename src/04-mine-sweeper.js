186c768e737f5ad6ace7845a54fe05d0431a9e88
wonderLuc <workmaildo@yandex.ru>
2021-04-10 22:19:21 +0500

    feat: slove task 04

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,28 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function minesweeper(matrix) {
+  return matrix.map((arr, arrIndex) => {
+    const line = arr.map((el, index) => {
+      // Split env to three varible
+      let linePrev = [];
+      let lineCurr = [];
+      let lineNext = [];
+      if (matrix[arrIndex - 1]) {
+        linePrev = [matrix[arrIndex - 1][index - 1],
+          matrix[arrIndex - 1][index],
+          matrix[arrIndex - 1][index + 1]];
+      }
+      if (matrix[arrIndex + 1]) {
+        lineNext = [matrix[arrIndex + 1][index - 1],
+          matrix[arrIndex + 1][index], matrix[arrIndex + 1][index + 1]];
+      }
+      lineCurr = [arr[index - 1], arr[index + 1]];
+      const enviroment = [...linePrev, ...lineCurr, ...lineNext];
+      return enviroment.filter((mine) => mine === true).length;
+    });
+    return line;
+  });
 }
 
 module.exports = minesweeper;

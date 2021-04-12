8023df8aa49af1fa0030fc485a387dfc406b9c95
wonderLuc <workmaildo@yandex.ru>
2021-04-12 16:17:23 +0500

    feat: slove task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,33 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let current;
+  let count = 1;
+  let result = '';
+  str.split('').map((el, index, arr) => {
+    if (!current) {
+      current = el;
+      return el;
+    }
+    if (current === el) {
+      count++;
+    }
+    if (current !== el && (index + 1) === arr.length) {
+      result += `${count > 1 ? count : ''}${arr[index - 1]}`;
+      current = el;
+      count = 1;
+      result += `${count > 1 ? count : ''}${arr[index]}`;
+      return el;
+    }
+    if (current !== el || (index + 1) === arr.length) {
+      result += `${count > 1 ? count : ''}${arr[index - 1]}`;
+      current = el;
+      count = 1;
+    }
+    return el;
+  });
+  return result;
 }
 
 module.exports = encodeLine;
